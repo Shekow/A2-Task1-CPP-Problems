66d7639a8c9d893029e0a309a14cac46c81a939a
Ahmed Yosry <ahmedyosry1014@gmail.com>
2023-11-01 10:40:33 +0200

    Added problem 1 file

---
+++ A2_S21_20221014_1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define each auto &
+#define endl '\n'
+#define pll pair<ll, ll>
+#define all(v) v.begin(), v.end()
+#define INF LONG_LONG_MAX
+#define Outsiders freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+using namespace std;
+// File: A2_S21_20221014_1.cpp
+// Purpose: Printing sentences with correct capitalization and punctuation
+// Author: Ahmed Yosry Saad
+// Section: 21
+// ID: 20221014
+// TA: Rana Abdelkader
+// Date: 1 Nov 2023
+
+
+
+
+int main(){
+        string s ;
+        string sentence="";
+        bool started = false;
+        while (cin >> s ){
+            for (int i = 0; i < s.size(); ++i) {
+                s[i]= tolower(s[i]);
+            }
+            if (started){
+                if (s==","||s=="."||s==":"){}
+                else if (s[0]==','||s[0]==':'){char tmp = s[0];s[0]=' ';s=tmp+s;}
+                else sentence+=" ";
+
+                for (int i = 0; i < s.size(); ++i) {
+                    if (i&&s[i]==','&& tolower(s[i-1])>='a'&& tolower(s[i+1])<='z'&&i+1!=s.size()){
+                        s.insert(i+1," ");
+
+                    }
+                }
+            }
+            sentence+=s;
+            started=true;
+
+
+            if (sentence[sentence.size()-1]=='.'){sentence[0]= toupper(sentence[0]);cout << sentence;return 0;}
+        }
+
+
+}
